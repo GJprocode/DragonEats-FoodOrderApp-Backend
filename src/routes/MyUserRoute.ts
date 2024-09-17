f50b852c36f4b26225e30dcedf4c2bfcde7c0d96
Gert Johannes Fourie <132646907+GJprocode@users.noreply.github.com>
2024-09-17 17:03:42 +0700

    latest

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -7,14 +7,9 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateMyUser);
-=======
-router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);  // Protected
-router.post("/", jwtCheck, MyUserController.createCurrentUser);  // Protected
-router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateMyUser);  // Protected
->>>>>>> 6c92afd (fix auth issue localhost issue render.com)
+ // Protected
 
 export default router;
